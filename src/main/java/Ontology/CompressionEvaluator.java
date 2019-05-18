6c8f7217cdf02092a74ae83c75eab7694eba3aa6
philipfrerk <philip.frerk@icloud.com>
2019-05-18 16:37:26 +0200

    ont wird zum graph selbst hinzugefügt

--- src/main/java/Ontology/CompressionEvaluator.java
+++ src/main/java/Ontology/CompressionEvaluator.java
@@ -4,7 +4,6 @@ import Util.Util;
 import compressionHandling.CompressionResult;
 import compressionHandling.CompressionStarter;
 import compressionHandling.GraphRePairStarter;
-import org.apache.jena.base.Sys;
 import org.apache.jena.rdf.model.Model;
 
 import java.io.File;
@@ -12,6 +11,7 @@ import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.*;
 
 public class CompressionEvaluator {
@@ -45,26 +45,25 @@ public class CompressionEvaluator {
         mapActiveFeatures.put(TRANSITIVE, false);
         mapActiveFeatures.put(EQUIVALENT, false);
         mapActiveFeatures.put(EVERYTHING, false);
-
     }
 
 
     public static final Dataset dataset = Dataset.DB_PEDIA;
 
-    public enum Dataset{
+    public enum Dataset {
         DB_PEDIA, WORDNET
     }
 
     //----------End configurations------------------------------------
 
 
-
-    private static final List<String> triplesForOntology = new ArrayList<>();
     private static final CompressionStarter cs = new GraphRePairStarter(6);
 
 
     private static List<CompressionResult> evaluateEuivReplacement(List<String> files, String ontology, File fileResult) {
         //TODO: hier hab ich das nicht mit den ontology relevanten predicates gemacht
+        List<String> triplesForOntology = new ArrayList<>();
+
         List<String> filesManipulated = new ArrayList<>();
         LinkedHashMap<String, List<String>> euivalentProperties = OntologyEvaluator.getAllEuivalentProperties(Util.getModelFromFile(ontology));
         for (String fileOriginal : files) {
@@ -78,11 +77,12 @@ public class CompressionEvaluator {
             filesManipulated.add(fileManipulated.getAbsolutePath());
         }
 
-        return evaluateCompression(filesManipulated, fileResult, true);
+        return evaluateCompression(filesManipulated, fileResult, true, triplesForOntology);
     }
 
     private static List<CompressionResult> evaluateInverseMaterialization(List<String> files, String ontology, File fileResult, boolean addEdges) {
         List<String> filesManipulated = new ArrayList<>();
+        List<String> triplesForOntology = new ArrayList<>();
         for (String fileOriginal : files) {
             Model model = Util.getModelFromFile(fileOriginal);
             int numReplacements = DataReplacer.materializeAllInverseDBPediaPredicates(model, Util.getModelFromFile(ontology), addEdges, triplesForOntology);
@@ -94,11 +94,13 @@ public class CompressionEvaluator {
             filesManipulated.add(fileManipulated.getAbsolutePath());
         }
 
-        return evaluateCompression(filesManipulated, fileResult, true);
+        return evaluateCompression(filesManipulated, fileResult, true, triplesForOntology);
     }
+
 
     private static List<CompressionResult> evaluateSymmetricMaterialization(List<String> files, String ontology, File fileResult, boolean addEdges) {
         List<String> filesManipulated = new ArrayList<>();
+        List<String> triplesForOntology = new ArrayList<>();
         for (String fileOriginal : files) {
             Model model = Util.getModelFromFile(fileOriginal);
             int numReplacements = DataReplacer.materializeAllSymmetricDBPediaPredicates(model, Util.getModelFromFile(ontology), addEdges, triplesForOntology);
@@ -110,11 +112,12 @@ public class CompressionEvaluator {
             filesManipulated.add(fileManipulated.getAbsolutePath());
         }
 
-        return evaluateCompression(filesManipulated, fileResult, true);
+        return evaluateCompression(filesManipulated, fileResult, true, triplesForOntology);
     }
 
     private static List<CompressionResult> evaluateTransitiveDeMaterialization(List<String> files, String ontology, File fileResult, boolean addEdges) {
         List<String> filesManipulated = new ArrayList<>();
+        List<String> triplesForOntology = new ArrayList<>();
         for (String fileOriginal : files) {
             Model model = Util.getModelFromFile(fileOriginal);
             int numReplacements = DataReplacer.dematerializeAllTransitivePredicates(model, addEdges, triplesForOntology);
@@ -126,13 +129,13 @@ public class CompressionEvaluator {
             filesManipulated.add(fileManipulated.getAbsolutePath());
         }
 
-        return evaluateCompression(filesManipulated, fileResult, true);
+        return evaluateCompression(filesManipulated, fileResult, true, triplesForOntology);
     }
 
     private static List<CompressionResult> evaluateEverything(List<String> files, String ontology, File fileResult) {
         List<String> filesManipulated = new ArrayList<>();
         Model modelOnt = Util.getModelFromFile(ontology);
-
+        List<String> triplesForOntology = new ArrayList<>();
         for (String fileOriginal : files) {
             Model model = Util.getModelFromFile(fileOriginal);
             int numReplacements = 0;
@@ -145,11 +148,30 @@ public class CompressionEvaluator {
             Util.writeModelToFile(fileManipulated, model);
             filesManipulated.add(fileManipulated.getAbsolutePath());
         }
-        return evaluateCompression(filesManipulated, fileResult, true);
+        return evaluateCompression(filesManipulated, fileResult, true, triplesForOntology);
     }
 
+    private static void appendRelevantOntTriplesToFile(String file, List<String> triplesForOntology) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String triple : triplesForOntology) {
+            stringBuilder.append(triple);
+            stringBuilder.append("\n");
+        }
+        try {
+            Files.write(Paths.get(file), stringBuilder.toString().getBytes(), StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-    private static List<CompressionResult> evaluateCompression(List<String> files, File fileResult, boolean deleteFiles) {
+    private static List<CompressionResult> evaluateCompression(List<String> files, File fileResult, boolean deleteFiles, List<String> triplesForOntology) {
+        if (triplesForOntology != null && !triplesForOntology.isEmpty()) {
+            for (String file : files) {
+                appendRelevantOntTriplesToFile(file, triplesForOntology);
+            }
+        }
+
+
         // TODO: sinnvoll?
         final boolean addDictSize = true;
 
@@ -246,9 +268,9 @@ public class CompressionEvaluator {
         return smallFiles;
     }
 
-    private static CompressionResult compressRelevantTriplesOfOntology() throws IOException {
+    private static CompressionResult compressRelevantTriplesOfOntology(List<String> triplesForOntology) throws IOException {
         StringBuilder stringBuilder = new StringBuilder();
-        for(String triple: triplesForOntology){
+        for (String triple : triplesForOntology) {
             stringBuilder.append(triple);
             stringBuilder.append("\n");
         }
@@ -258,18 +280,16 @@ public class CompressionEvaluator {
         return cs.compress(ontology, null, true);
     }
 
-    private static void printEvaluationResults(Map<String,List<CompressionResult>> mapResults, CompressionResult resultOntology){
+    private static void printEvaluationResults(Map<String, List<CompressionResult>> mapResults) {
         long compressedSizeOrignal = mapResults.get(ORIGINAL).get(0).getCompressedSize();
 
         System.out.println("\n\nevalutation result\n");
-        System.out.println("orginal compr size: "+compressedSizeOrignal);
+        System.out.println("orginal compr size: " + compressedSizeOrignal);
 
-        for(String feature : mapResults.keySet()){
-            if(!feature.equals(ORIGINAL)){
+        for (String feature : mapResults.keySet()) {
+            if (!feature.equals(ORIGINAL)) {
                 long compressedSize = mapResults.get(feature).get(0).getCompressedSize();
-                System.out.print(feature+" compr size: "+compressedSize);
-                long sizeWithOnt = compressedSize+resultOntology.getCompressedSize();
-                System.out.println("  (with ontology: "+ sizeWithOnt + " )");
+                System.out.print(feature + " compr size: " + compressedSize);
             }
         }
 
@@ -281,28 +301,27 @@ public class CompressionEvaluator {
         List<String> dataFiles = prepareDataFiles();
         final String ontology = "dbpedia_2015-04.owl";
 
-        Map<String,List<CompressionResult>> mapResults = new LinkedHashMap<>();
+        Map<String, List<CompressionResult>> mapResults = new LinkedHashMap<>();
 
-        mapResults.put(ORIGINAL,evaluateCompression(dataFiles, resultFiles.get(0), false));
+        mapResults.put(ORIGINAL, evaluateCompression(dataFiles, resultFiles.get(0), false, null));
 
-        if(mapActiveFeatures.get(EQUIVALENT)) {
-            mapResults.put(EQUIVALENT,evaluateEuivReplacement(dataFiles, ontology, resultFiles.get(1)));
+        if (mapActiveFeatures.get(EQUIVALENT)) {
+            mapResults.put(EQUIVALENT, evaluateEuivReplacement(dataFiles, ontology, resultFiles.get(1)));
         }
-        if(mapActiveFeatures.get(SYMMETRIC)) {
-            mapResults.put(SYMMETRIC,evaluateSymmetricMaterialization(dataFiles, ontology, resultFiles.get(2), mapMaterialization.get(SYMMETRIC)));
+        if (mapActiveFeatures.get(SYMMETRIC)) {
+            mapResults.put(SYMMETRIC, evaluateSymmetricMaterialization(dataFiles, ontology, resultFiles.get(2), mapMaterialization.get(SYMMETRIC)));
         }
-        if(mapActiveFeatures.get(TRANSITIVE)) {
-            mapResults.put(TRANSITIVE,evaluateTransitiveDeMaterialization(dataFiles, ontology, resultFiles.get(3), mapMaterialization.get(TRANSITIVE)));
+        if (mapActiveFeatures.get(TRANSITIVE)) {
+            mapResults.put(TRANSITIVE, evaluateTransitiveDeMaterialization(dataFiles, ontology, resultFiles.get(3), mapMaterialization.get(TRANSITIVE)));
         }
-        if(mapActiveFeatures.get(INVERSE)) {
-            mapResults.put(INVERSE,evaluateInverseMaterialization(dataFiles, ontology, resultFiles.get(4), mapMaterialization.get(INVERSE)));
+        if (mapActiveFeatures.get(INVERSE)) {
+            mapResults.put(INVERSE, evaluateInverseMaterialization(dataFiles, ontology, resultFiles.get(4), mapMaterialization.get(INVERSE)));
         }
-        if(mapActiveFeatures.get(EVERYTHING)) {
-            mapResults.put(EVERYTHING,evaluateEverything(dataFiles, ontology, resultFiles.get(5)));
+        if (mapActiveFeatures.get(EVERYTHING)) {
+            mapResults.put(EVERYTHING, evaluateEverything(dataFiles, ontology, resultFiles.get(5)));
         }
 
-        CompressionResult resultOntology = compressRelevantTriplesOfOntology();
-        printEvaluationResults(mapResults,resultOntology);
+        printEvaluationResults(mapResults);
 
         // clean up
         for (String dataFile : dataFiles) {
